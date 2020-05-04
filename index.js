1b98ad5d38c89ff981f47506f92f5a677c590840
nitro-neal <groessler24@gmail.com>
2020-05-04 16:26:42 -0500

    Adding requires

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 const PORT = process.env.PORT || 5000;
 
-async function ls() {
+async function ls(dir) {
   try {
-    const { stdout, stderr } = await exec("ls");
+    const { stdout, stderr } = await exec("ls " + dir);
     console.log("stdout:", stdout);
     console.log("stderr:", stderr);
+    return stdout;
   } catch (err) {
     console.error(err);
   }
@@ -17,8 +20,8 @@ express()
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
-  .get("/ls", (req, res) => {
-    let lsValue = await ls()
-    res.json({msg:lsValue});
+  .get("/ls/:dir", async (req, res) => {
+    let lsValue = await ls(req.params.dir);
+    res.json({ msg: lsValue });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
